bed4a5975db83143be363054212bf8ae7a4f0b8d
Mehdi Taherabadi <MehdiTaherabadi@outlook.com>
2020-09-14 17:21:59 +0430

    Add Project Context

---
+++ Barname/CleanArch.Infra.Data/Context/BaranameDBContext.cs
@@ -0,0 +1,15 @@
+﻿using CleanArch.Domin.Modals;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class BaranameDBContext: DbContext
+    {
+        public BaranameDBContext(DbContextOptions<BaranameDBContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
